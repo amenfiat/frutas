828515ff4cfc03b50b5b542477c2e6ccebb8e824
jquilismal86 <wjquilismalp@gmail.com>
2021-11-29 21:56:27 -0500

    Added Conocenos.js changes

--- frontend/src/pages/Conocenos/Conocenos.js
+++ frontend/src/pages/Conocenos/Conocenos.js
@@ -1,21 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
 import { Slides } from "../../components/carousel/Slides";
 
 function Conocenos() {
-  return (
-    <Slides />
-=======
-
-function Conocenos() {
-  return (
-    <div className="container">
-      <p>
-        Conocenos! <span> Es un gusto! </span>
-      </p>
-    </div>
->>>>>>> abel
-  );
+  return <Slides />;
 }
 
 export { Conocenos };
