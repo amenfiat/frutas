3b574034e3bdfca92ef119c6fcc9cc9f40c220a9
jquilismal86 <wjquilismalp@gmail.com>
2021-12-01 23:12:07 -0500

    Final environment config

--- backend/src/db/firebase.js
+++ backend/src/db/firebase.js
@@ -4,18 +4,6 @@ try {
   serviceAccount = require("./frutasexportacion-9c6db-414d96644e62.json"); //* => Firebase KEY
 } catch (error) {
   console.log("File not found, so use environment variables");
-  /*serviceAccount = {
-    "type": process.env.TYPE,
-    "project_id": process.env.PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-    "client_email": process.env.CLIENT_EMAIL,
-    "client_id": process.env.CLIENT_ID,
-    "auth_uri": process.env.AUTH_URI,
-    "token_uri": process.env.TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
-  };*/
   serviceAccount = JSON.parse(process.env.SERVICEACCOUNT);
 }
 
